6ce24fe02e1817e03ed8fa6c8c6442e21827c199
toka <tokaalsabbagh@gmail.com>
2024-10-15 10:01:38 -0700

    after push color

---
+++ lib/color_.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class color_{
+  static const Color black =  Colors.black;
+  static const Color white =  Colors.white;
+  static const Color background =  Colors.black87;
+  static const Color background2 =  Colors.white12;
+  static const Color gray =  Colors.grey;
+  static const Color button =  Colors.white24;
+  static const Color font =  Colors.white70;
+
+}
