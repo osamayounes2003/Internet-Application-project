059987f24f0b90664c08ca7342c57f0177ab0ca7
toka <tokaalsabbagh@gmail.com>
2024-10-28 13:03:37 -0700

    add file to group

--- lib/user/UploadFile/UploadFile_Screen.dart
+++ lib/user/UploadFile/UploadFile_Screen.dart
@@ -1,99 +1,103 @@
-import 'package:file_manager_internet_applications_project/CustomComponent/CustomButton.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../CustomComponent/BaseScreen.dart';
 import 'UploadFile_Controller.dart';
 
 class UploadFile_Screen extends StatelessWidget {
-  final FileUploadController fileUploadController =
-      Get.put(FileUploadController());
+  final FileUploadController fileUploadController = Get.put(FileUploadController());
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 600,
-              width: double.infinity,
-              color: Colors.black87,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      "assets/SelectFile.png",
-                      height: 150,
-                      width: 150,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 20.0),
-                      child: Text(
-                        "Choose the file you want to upload",
-                        style: TextStyle(color: Colors.white70, fontSize: 25),
-                        textAlign: TextAlign.center,
+    final groupId = Get.arguments != null && Get.arguments.containsKey('groupId')
+        ? Get.arguments['groupId']
+        : null;
+
+    return BaseScreen(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.6,
+                width: double.infinity,
+                color: Colors.black87,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "assets/SelectFile.png",
+                        height: 150,
+                        width: 150,
                       ),
-                    ),
-                    Padding(
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20.0),
+                        child: Text(
+                          "Choose the file you want to upload",
+                          style: TextStyle(color: Colors.white70, fontSize: 25),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Padding(
                         padding: const EdgeInsets.symmetric(vertical: 10.0),
-                        child: CustomElevatedButton(
-                          title: "Select File",
-                          color: Colors.white24,
+                        child: ElevatedButton(
                           onPressed: () async {
                             await fileUploadController.selectFile();
                           },
-                        )
+                          child: Text("Select File"),
                         ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Container(
+                  padding: EdgeInsets.all(40),
+                  width: MediaQuery.of(context).size.width > 600 ? 400 : 300,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Center(
+                    child: Obx(() {
+                      return Text(
+                        fileUploadController.selectedFileName.value.isNotEmpty
+                            ? fileUploadController.selectedFileName.value
+                            : "No file selected",
+                        style: TextStyle(color: Colors.black87),
+                      );
+                    }),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () async {
+                  await fileUploadController.uploadFile(groupId);
+                },
+                child: Column(
+                  children: [
+                    Image.asset(
+                      "assets/UploadFile.png",
+                      height: 100,
+                      width: 100,
+                    ),
+                    Text("Press to upload"),
                   ],
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20.0),
-              child: Container(
-                padding: EdgeInsets.all(40),
-                width: 300,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 1.0,
-                  ),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Center(
-                  child: Obx(() {
-                    return Text(
-                      fileUploadController.selectedFileName.value.isNotEmpty
-                          ? fileUploadController.selectedFileName.value
-                          : "No file selected",
-                      style: TextStyle(color: Colors.black87),
-                    );
-                  }),
-                ),
-              ),
-            ),
-            InkWell(
-              onTap: () async {
-                await fileUploadController.uploadFile();
-              },
-              child: Column(
-                children: [
-                  Image.asset(
-                    "assets/UploadFile.png",
-                    height: 100,
-                    width: 100,
-                  ),
-                  Text("Press to upload"),
-                ],
-              ),
-            ),
-            Obx(() {
-              return fileUploadController.isUploading.value
-                  ? CircularProgressIndicator()
-                  : SizedBox.shrink();
-            }),
-          ],
+              Obx(() {
+                return fileUploadController.isUploading.value
+                    ? CircularProgressIndicator()
+                    : SizedBox.shrink();
+              }),
+            ],
+          ),
         ),
       ),
     );
--- lib/user/UploadFile/UploadFile_Services.dart
+++ lib/user/UploadFile/UploadFile_Services.dart
@@ -1,21 +1,16 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
 import '../../SharedPreferences/shared_preferences_service.dart';
 import 'UploadFile_Model.dart';
 
 class FileUploadService {
   final SharedPreferencesService sharedPreferencesService = SharedPreferencesService();
 
-  Future<FileModel?> uploadFile(String filePath) async {
+  Future<FileModel?> uploadFileBytes(List<int> fileBytes, String fileName, int groupId) async {
     String? token = await sharedPreferencesService.getToken();
-    int? userId = await sharedPreferencesService.getUserId();
 
     if (token == null) {
       throw Exception('No token found');
-    }
-    if (userId == null) {
-      throw Exception('No user ID found');
     }
 
     var headers = {
@@ -24,9 +19,8 @@ class FileUploadService {
     };
 
     var request = http.MultipartRequest('POST', Uri.parse('http://195.88.87.77:8888/api/v1/files'));
-    request.fields['userId'] = userId.toString();
-    request.fields['status'] = 'UNAVAILABLE';
-    request.files.add(await http.MultipartFile.fromPath('file', filePath));
+    request.fields['folderId'] = groupId.toString();
+    request.files.add(http.MultipartFile.fromBytes('file', fileBytes, filename: fileName));
     request.headers.addAll(headers);
 
     http.StreamedResponse response = await request.send();
